564a003124d66d2265b8556db873f11ab1bcff1e
novemberjr <jr.november@gmail.com>
2022-05-26 19:53:04 +0700

    Upload shop

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
+import 'normalize.css';
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import './styles/main.scss';
 import './index.html';
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [ Navigation, Pagination ]
+  });
